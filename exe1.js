486c62d4e9b14d8ec1d19de2ede5c702a3da652e
Renata Claudino <saraiva.claudino@gmail.com>
2025-08-21 10:40:10 -0300

    Exercicio 1

---
+++ exe1.js
@@ -0,0 +1,19 @@
+function bubbleSort(vetor) {
+    fpr ( i =0; i <vetor.length; i++) {
+        var aux
+        for (j = 0; j < vetor.length - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+            }
+        }
+}
+}
+vet = []
+for (let i = 0; i < 10; i++) {
+    vet[i] = Math.round(Math.random() * (100-1) + 1);   
+}
+console.log(vet)
+bubbleSort(vet)
+console.log(vet)
