13b7ff72de80219ae7b69f703336239552ae2538
Renata Claudino <saraiva.claudino@gmail.com>
2025-08-28 09:27:20 -0300

    Correção exe seleção direta

--- selecaoDireta.js
+++ selecaoDireta.js
@@ -1,26 +1,18 @@
 function selecaoDireta(vetor) {
-    let tam = vetor.length;
-
-    for (let i = 0; i < tam - 1; i++) {
-        let menor = i;
-
-        for (let j = i + 1; j < n; j++) {
-            if (vetor[j] < vetor[menor]) {
-                menor = j;
+    for (var u = 0; i <vetor.length; i++){
+        var menor = vetor [i]
+        var pos = i
+        for (var j = i+1; j <vetor.length; j++){
+            if (vetor[j] <menor){
+                menor = vetor[j]
+                pos = j
             }
         }
-
-        if (minIndex !== i) {
-            let aux = vetor[i];
-            vetor[i] = vetor[menor];
-            vetor[menor] = aux;
-        }
+        var aux = vetor[i]
+        vetor[i] = vetor[pos]
+        vetor[pos] = aux
     }
 }
-
-let vet = [25, 57, 48, 37, 12, 92, 86, 33];
-console.log("Antes da ordenação:", vet);
-
-selecaoDireta(vet);
-
-console.log("Depois da ordenação:", vet);
+let num = [25, 57 ,48 ,37 ,12 ,92 ,86, 33]
+selecaoDireta(nums)
+console.log(nums)
